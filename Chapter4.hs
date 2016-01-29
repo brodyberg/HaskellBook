3ad0545286fd0f07b0f9997f9ce196f744de3602
Brody Berg <brodyberg@gmail.com>
2016-01-29 15:50:28 -0800

    Problem 9

--- Chapter4.hs
+++ Chapter4.hs
@@ -46,3 +46,15 @@ isPalindrome "Go hang a salami; I'm a lasagna hog!"
 -- we don't know yet how to copy/paste multi-line expressions
 -- into the REPL so we can't do a test using foldl which
 -- would prove our code vs. strings above. 
+
+--9 use if/else to return absolute value of a number
+absolute :: Num => a : a -> a
+absolute x = if x > 0 then x else -x
+    
+absolute 9
+absolute (-9)
+
+-- 10 fill in definition for: 
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f = 
+    
