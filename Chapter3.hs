6e4ce8197f6368f280dd887d807791f79a58555a
Brody Berg <brodyberg@gmail.com>
2016-01-29 12:03:51 -0700

    Chapter 3 work

---
+++ Chapter3.hs
@@ -0,0 +1,53 @@
+module Chapter3 where
+    
+-- pg 106
+    
+-- area d = pi * (r * r)
+-- r = d / 2
+
+-- pg 109
+-- area      d = pi * (r * r)
+--     where r = d / 2
+--     
+--     
+-- -- pg 109
+-- Prelude> :t (++)
+-- (++) :: [a] -> [a] -> [a]
+-- Prelude> ++ [1, 2, 3] [4, 5, 6]
+-- 
+-- <interactive>:18:1: parse error on input `++'
+-- Prelude> [1, 2, 3] ++ [4, 5, 6]
+-- [1,2,3,4,5,6]
+-- Prelude> (++) [1, 2, 3] [4, 5, 6]
+-- [1,2,3,4,5,6]
+-- Prelude> :t '<3'
+-- 
+-- <interactive>:1:2: parse error on input `<'
+-- Prelude> :t "<3"
+-- "<3" :: [Char]
+-- Prelude> '<3
+-- 
+-- <interactive>:23:2: parse error on input `<'
+-- Prelude> '<3'
+-- 
+-- <interactive>:24:2: parse error on input `<'
+-- Prelude> "<3" ++ ' Haskell'
+-- 
+-- <interactive>:25:9:
+--     Syntax error on 'Haskell'
+--     Perhaps you intended to use TemplateHaskell
+--     In the Template Haskell quotation 'Haskell'
+-- Prelude> "<3" ++ " Haskell"
+-- "<3 Haskell"
+-- Prelude> concat ["<3", " Haskell"]
+-- "<3 Haskell"
+-- Prelude> (++) "<3" " Haskell"]
+-- 
+-- <interactive>:28:21: parse error on input `]'
+-- Prelude> (++) "<3" " Haskell"
+-- "<3 Haskell"
+
+-- pg 115
+
+take ((length "awesome!") -1) "awesome!"
+take 7 "awesome!"
