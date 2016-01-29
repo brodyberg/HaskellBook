ece9bf977f9de5f45fb89a78a94d37307a838854
Brody Berg <brodyberg@gmail.com>
2016-01-29 10:14:52 -0700

    Module, implement byPi
    The way I figured out the type signature of byPi was to define it in
    ghci and then do :t byPi and then copy that type signature to the *.hs
    file

--- Chapter2.hs
+++ Chapter2.hs
@@ -1,4 +1,4 @@
-module Hello
+module Hello where
 
 sayHello :: String -> IO ()
 sayHello x = putStrLn ("hello " ++ x ++ "!") 
@@ -9,5 +9,7 @@ triple x = x * 3
 square :: Integer -> Integer
 square x = x * x
 
-byPi :: Integer -> Fractional Integer
-byPi x = 3.14 * (square x)
+-- byPi :: Integer -> Integer
+-- byPi :: Num -> Num
+byPi :: Fractional a => a -> a
+byPi x = 3.14 * (x * x)
